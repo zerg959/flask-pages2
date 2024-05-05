e38ddb12ddcd906ec4bc4315b04e8fc6725b38b8
zerg959 <zerg959@yandex.ru>
2024-05-06 01:33:59 +0300

    REFACTOR: auth

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from flask import Flask, render_template, redirect, url_for, request, session
-import sqlite3
+from flask import (
+    Flask, render_template,
+    redirect, url_for, request,
+    flash, session
+    )
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import (
+    LoginManager, UserMixin,
+    login_user, logout_user,
+    login_requred, current_user
+    )
+from sqlalchemy import func
+import os
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
 
+app.config["SECRET_KEY"] = 'your_secret_key'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(base_dir, 'database.db')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+db.init_app(app)
 
 # Устанавливаем соединение с Базой Данных
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+# def get_db_connection():
+#     conn = sqlite3.connect('database.db')
+#     conn.row_factory = sqlite3.Row
+#     return conn
 
 @app.route('/')
 def home():
